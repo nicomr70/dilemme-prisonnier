a6e6859e3a91f84841feb9b3a1865e0ce5aab376
Lxsvn98 <alexis.yvon@gmail.com>
2021-11-17 12:35:40 +0100

    Improve RandomStrategy for inheritance

--- src/main/java/com/example/models/strategy/RandomStrategy.java
+++ src/main/java/com/example/models/strategy/RandomStrategy.java
@@ -1,5 +1,6 @@
 package com.example.models.strategy;
 
+import com.example.models.SingleRandom;
 import com.example.models.player.Player;
 import com.example.models.player.PlayerChoice;
 
@@ -7,10 +8,27 @@ import java.util.Random;
 
 /** Stratégie Aléatoire */
 class RandomStrategy implements IStrategy {
-    protected final Random random = new Random();
+    private final double OPPOSITE_CHOICE_DEFAULT_PROBABILITY = 0.1;
+
+    protected final Random random = SingleRandom.getInstance();
+
+    public final PlayerChoice chooseRandomly(PlayerChoice defaultChoice, double oppositeChoiceProbability) {
+        PlayerChoice oppositeChoice = defaultChoice.getOpposite();
+        if (oppositeChoiceProbability <= 0.0 || oppositeChoiceProbability > 1.0) {
+            oppositeChoiceProbability = OPPOSITE_CHOICE_DEFAULT_PROBABILITY;
+        }
+        if (random.nextDouble() < oppositeChoiceProbability) {
+            return oppositeChoice;
+        }
+        return defaultChoice;
+    }
+
+    public final PlayerChoice chooseRandomly(PlayerChoice defaultChoice) {
+        return chooseRandomly(defaultChoice, OPPOSITE_CHOICE_DEFAULT_PROBABILITY);
+    }
 
     @Override
     public PlayerChoice execute(int turnCount, Player player, Player otherPlayer) {
-        return random.nextBoolean() ? PlayerChoice.COOPERATE : PlayerChoice.DEFECT;
+        return chooseRandomly(PlayerChoice.COOPERATE, 0.5);
     }
 }
