505ce5b333419a764419a8a0dfa7896166e63010
Lxsvn98 <alexis.yvon@gmail.com>
2021-11-17 12:30:33 +0100

    Add PlayerChoice getOpposite method

--- src/main/java/com/example/models/player/PlayerChoice.java
+++ src/main/java/com/example/models/player/PlayerChoice.java
@@ -1,7 +1,16 @@
 package com.example.models.player;
 
+import com.example.models.SingleRandom;
+
 public enum PlayerChoice {
     DEFECT,
     COOPERATE,
-    NONE
+    NONE;
+
+    public final PlayerChoice getOpposite() {
+        if (this == NONE) {
+            return SingleRandom.getInstance().nextBoolean() ? COOPERATE : DEFECT;
+        }
+        return this == COOPERATE ? DEFECT : COOPERATE;
+    }
 }
