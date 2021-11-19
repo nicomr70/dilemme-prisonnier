35d584cc87dc209212c7ed4e19a7fd7c0ca01dc0
Lxsvn98 <alexis.yvon@gmail.com>
2021-11-19 02:12:22 +0100

    Fix TestHttp code smells

--- src/main/java/fr/uga/miage/m1/tests/TestHttp.java
+++ src/main/java/fr/uga/miage/m1/tests/TestHttp.java
@@ -1,17 +1,7 @@
 package fr.uga.miage.m1.tests;
 
-import java.util.ArrayList;
-import java.util.Map;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestHttp {
 
     @GetMapping("/ping")
-    String pong() {
+    public String pong() {
       return "pong";
     }
 
     @GetMapping("/michelle")
-    String test(){
+    public String test(){
         return "aah";
     }
 }
