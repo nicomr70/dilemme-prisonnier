acd79c1daacfdacd41dd0209ddada7486e12ace7
Lxsvn98 <alexis.yvon@gmail.com>
2021-11-20 18:57:11 +0100

    Improve StrategyFactory behaviour regarding StrategyType

--- src/main/java/fr/uga/miage/m1/models/strategy/StrategyFactory.java
+++ src/main/java/fr/uga/miage/m1/models/strategy/StrategyFactory.java
@@ -1,26 +1,16 @@
 package fr.uga.miage.m1.models.strategy;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class StrategyFactory {
     private StrategyFactory() {}
 
-    public static IStrategy getStrategyFromType(StrategyType strategyType) {
+    public static IStrategy getStrategyFromType(StrategyType strategyType)
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException
+    {
         if (strategyType == null) {
             return new RandomStrategy();
         }
-        return switch (strategyType) {
-            case COOPERATE -> new CooperateStrategy();
-            case DEFECT -> new DefectStrategy();
-            case TIT_FOR_TAT -> new TitForTatStrategy();
-            case SUSPICIOUS_TIT_FOR_TAT -> new SuspiciousTitForTatStrategy();
-            case GENEROUS_TIT_FOR_TAT -> new GenerousTitForTatStrategy();
-            case RANDOM_TIT_FOR_TAT -> new RandomTitForTatStrategy();
-            case NAIVE_PROBER -> new NaiveProberStrategy();
-            case GRIM_TRIGGER -> new GrimTriggerStrategy();
-            case SOFT_GRUDGER -> new SoftGrudgerStrategy();
-            case GRADUAL -> new GradualStrategy();
-            case PAVLOV -> new PavlovStrategy();
-            case RANDOM_PAVLOV -> new RandomPavlovStrategy();
-            case RANDOM -> new RandomStrategy();
-        };
+        return strategyType.getStrategyClass().getDeclaredConstructor().newInstance();
     }
 }
--- src/main/java/fr/uga/miage/m1/models/strategy/StrategyType.java
+++ src/main/java/fr/uga/miage/m1/models/strategy/StrategyType.java
@@ -1,27 +1,33 @@
 package fr.uga.miage.m1.models.strategy;
 
 public enum StrategyType {
-    COOPERATE("Toujours coopérer"),
-    DEFECT("Toujours trahir"),
-    RANDOM("Aléatoire"),
-    TIT_FOR_TAT("Donnant-donnant"),
-    SUSPICIOUS_TIT_FOR_TAT("Donnant-donnant soupçonneux"),
-    RANDOM_TIT_FOR_TAT("Donnant-donnant aléatoire"),
-    GENEROUS_TIT_FOR_TAT("Pacificateur naïf"),
-    NAIVE_PROBER("Sondeur naïf"),
-    GRIM_TRIGGER("Rancunier"),
-    SOFT_GRUDGER("Rancunier doux"),
-    GRADUAL("Graduel"),
-    PAVLOV("Pavlov"),
-    RANDOM_PAVLOV("Pavlov aléatoire");
+    COOPERATE("Toujours coopérer", CooperateStrategy.class),
+    DEFECT("Toujours trahir", DefectStrategy.class),
+    RANDOM("Aléatoire", RandomStrategy.class),
+    TIT_FOR_TAT("Donnant-donnant", TitForTatStrategy.class),
+    SUSPICIOUS_TIT_FOR_TAT("Donnant-donnant soupçonneux", SuspiciousTitForTatStrategy.class),
+    RANDOM_TIT_FOR_TAT("Donnant-donnant aléatoire", RandomTitForTatStrategy.class),
+    GENEROUS_TIT_FOR_TAT("Pacificateur naïf", GenerousTitForTatStrategy.class),
+    NAIVE_PROBER("Sondeur naïf", NaiveProberStrategy.class),
+    GRIM_TRIGGER("Rancunier", GrimTriggerStrategy.class),
+    SOFT_GRUDGER("Rancunier doux", SoftGrudgerStrategy.class),
+    GRADUAL("Graduel", GradualStrategy.class),
+    PAVLOV("Pavlov", PavlovStrategy.class),
+    RANDOM_PAVLOV("Pavlov aléatoire", RandomPavlovStrategy.class);
 
-    private String name;
+    private final String name;
+    private final Class<? extends IStrategy> strategyClass;
 
-    StrategyType(String name) {
+    StrategyType(String name, Class<? extends IStrategy> strategyClass) {
         this.name = name;
+        this.strategyClass = strategyClass;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Class<? extends IStrategy> getStrategyClass() {
+        return strategyClass;
     }
 }
