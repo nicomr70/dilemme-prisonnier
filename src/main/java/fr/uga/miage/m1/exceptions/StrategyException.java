7346e2e1e383794e97323877d183b45a46240fdd
Lxsvn98 <alexis.yvon@gmail.com>
2021-11-20 17:57:24 +0100

    Remove StrategyException unused constructor

--- src/main/java/fr/uga/miage/m1/exceptions/StrategyException.java
+++ src/main/java/fr/uga/miage/m1/exceptions/StrategyException.java
@@ -4,8 +4,4 @@ public class StrategyException extends Exception {
     public StrategyException(String message) {
         super(message);
     }
-
-    public StrategyException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
